aa9884427dfa023af78212cb9e39cace8dcc808b
Adam Probert <adamprobert@live.co.uk>
2018-11-10 19:11:48 +0000

    Split out websocket code to seperate function

--- main/backend.go
+++ main/backend.go
@@ -40,55 +40,7 @@ func main() {
 	}()
 
 	go func() {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte("{\"hello\": \"world\"}"))
-		})
-
-		// cors.Default() setup the middleware with default options being
-		// all origins accepted with simple methods (GET, POST). See
-		// documentation below for more options.
-		server, err := socketio.NewServer(nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		server.On("connection", func(so socketio.Socket) {
-			so.Emit("chat", "hello message")
-			log.Println("on connection")
-			so.On("chat", func(msg string) {
-				log.Println("recieved message", msg)
-			})
-			// Socket.io acknowledgement example
-			// The return type may vary depending on whether you will return
-			// For this example it is "string" type
-			so.On("chat message with ack", func(msg string) string {
-				return msg
-			})
-			so.On("disconnection", func() {
-				log.Println("disconnected from chat")
-			})
-		})
-		server.On("error", func(so socketio.Socket, err error) {
-			log.Println("error:", err)
-		})
-
-		mux.Handle("/socket.io/", server)
-		mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
-
-		// provide default cors to the mux
-		handler := cors.Default().Handler(mux)
-
-		c := cors.New(cors.Options{
-			AllowedOrigins:   []string{"*"},
-			AllowCredentials: true,
-		})
-
-		// decorate existing handler with cors functionality set in c
-		handler = c.Handler(handler)
-
-		log.Println("Serving at localhost:5000...")
-		log.Fatal(http.ListenAndServe(":5000", handler))
+		StartSocketServer()
 	}()
 
 	log.Print("This should run after listen finish")
@@ -99,6 +51,85 @@ func main() {
 	}
 
 }
+
+func NewSocket(so socketio.Socket) {
+
+	so.Join("chat")
+
+	so.Emit("chat", "hello message")
+	log.Println("on connection")
+	so.On("chat", func(msg string) {
+		log.Println("recieved message", msg)
+		so.Emit("chat", msg)
+	})
+	// Socket.io acknowledgement example
+	// The return type may vary depending on whether you will return
+	// For this example it is "string" type
+	so.On("chat message with ack", func(msg string) string {
+		return msg
+	})
+	so.On("disconnection", func() {
+		log.Println("disconnected from chat")
+	})
+}
+
+func StartSocketServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+	})
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+
+		so.Join("chat")
+
+		so.Emit("chat", "hello message")
+		log.Println("on connection")
+		so.On("chat", func(msg string) {
+			log.Println("recieved message", msg)
+			so.Emit("chat", msg)
+		})
+		// Socket.io acknowledgement example
+		// The return type may vary depending on whether you will return
+		// For this example it is "string" type
+		so.On("chat message with ack", func(msg string) string {
+			return msg
+		})
+		so.On("disconnection", func() {
+			log.Println("disconnected from chat")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	mux.Handle("/socket.io/", server)
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+
+	// provide default cors to the mux
+	handler := cors.Default().Handler(mux)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: false,
+	})
+
+	// decorate existing handler with cors functionality set in c
+	handler = c.Handler(handler)
+
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":5000", handler))
+
+}
+
 func ReturnClients(w http.ResponseWriter, r *http.Request) {
 	formattedStruct, _ := json.Marshal(GetClients())
 	fmt.Fprintln(w, string(formattedStruct))
